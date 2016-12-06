7223501dff5239775e0ffc07c00d498f014a2d63
Sophie <sophie.roch@gmail.com>
2016-12-06 16:41:29 +0100

    quit button and resolution restricted

--- Shades/Assets/CustomAssets/Scripts/System/SceneLoader.cs
+++ Shades/Assets/CustomAssets/Scripts/System/SceneLoader.cs
@@ -17,4 +17,8 @@ public class SceneLoader : MonoBehaviour {
     public void ChangeScene (string scene) {
         SceneManager.LoadScene(scene);
     }
+
+    public void Quit() {
+        Application.Quit();
+    }
 }
